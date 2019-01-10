82bf63039637a961231660737a732f8fb88647fa
clyhs <clygd@126.com>
2019-01-10 14:48:54 +0800

    ribbon Hystrix

--- springcloud-ribbon/src/main/java/com/springcloud/ribbon/service/HelloService.java
+++ springcloud-ribbon/src/main/java/com/springcloud/ribbon/service/HelloService.java
@@ -4,14 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class HelloService {
 
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HELLO/hello?name="+name,String.class);
+    }
+    
+    public String hiError(String name) {
+        return "hello,"+name+",sorry,SERVICE-HELLO error!";
     }
 
 
