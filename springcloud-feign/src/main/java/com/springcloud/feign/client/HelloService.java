6c6861b2f0c5a16647061d694311eb8c45b1b3ee
clyhs <clygd@126.com>
2019-01-10 14:41:43 +0800

    feign

---
+++ springcloud-feign/src/main/java/com/springcloud/feign/client/HelloService.java
@@ -0,0 +1,15 @@
+package com.springcloud.feign.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@FeignClient(value = "service-hello")
+public interface HelloService {
+	
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
+
